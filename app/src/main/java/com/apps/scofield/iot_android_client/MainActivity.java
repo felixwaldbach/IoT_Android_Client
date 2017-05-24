8c020ba551c5b753991d4c7e306d6a0da45deaf5
fsc0field <felix-waldbach@t-online.de>
2017-05-25 01:40:08 +0200

    Initial Commit

---
+++ app/src/main/java/com/apps/scofield/iot_android_client/MainActivity.java
@@ -0,0 +1,26 @@
+package com.apps.scofield.iot_android_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    Button btn2;
+
+    EditText et;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+        btn2 = (Button) findViewById(R.id.button2);
+        et = (EditText) findViewById(R.id.editText);
+
+
+    }
+}
